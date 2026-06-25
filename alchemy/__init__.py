a7746e79dae04a0b0032c4db169e0bd7abc0bac9
csng98 <csengekakula@gmail.com>
2026-06-25 13:03:09 +0200

    working on imports

---
+++ alchemy/__init__.py
@@ -0,0 +1,3 @@
+from .elements import create_air, create_earth
+from .potions import healing_potion as heal
+from .potions import strength_potion
---
+++ ft_transmutation_0.py
@@ -0,0 +1,5 @@
+import alchemy.transmutation.recipes
+
+
+if __name__ == "__main__":
+	print("=== Transmutation 0 ===")
