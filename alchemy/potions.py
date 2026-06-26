f140bb7c6beaa08881fc104885685d86eea48eec
csng98 <csengekakula@gmail.com>
2026-06-26 08:14:02 +0200

    tested distillation 0-1, fixed typo in potions

--- alchemy/potions.py
+++ alchemy/potions.py
@@ -3,7 +3,7 @@ from elements import create_fire, create_water
 
 
 def healing_potion() -> str:
-    return (f"Healing potioon brewed with "
+    return (f"Healing potion brewed with "
             f"'{create_earth()}' and '{create_air()}'")
 
 
